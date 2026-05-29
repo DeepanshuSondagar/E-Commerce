60e7edf1982a3095bef34459396b61cd17910601
Deepanshu Sondagar <deepsondagar7892@gmail.com>
2026-05-30 00:29:23 +0530

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import path from "path";  
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: resolve(__dirname, "..", ".env") });
-
 
 import express from "express";
 import cookieParser from "cookie-parser";
@@ -15,39 +15,33 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
- 
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit: "10mb"}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-// enable CORS for local frontend during development
-app.use(
-    cors({
-        origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-        credentials: true,
-    })
-);
-
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173", 
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
-app.use("/api/analytics",  analyticsRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
-
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));  
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));  
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
-
-
-export const getProfile = async(req,res)=>{
-
-}
