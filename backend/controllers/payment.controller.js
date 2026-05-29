00a09d7e95a5b745f87407311771abbcf3833a44
Deepanshu Sondagar <deepsondagar7892@gmail.com>
2026-05-30 01:15:36 +0530

    Project Completed

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -139,12 +139,13 @@ async function createNewCoupon(userId) {
     await Coupon.findOneAndDelete({ userId});
 
 
-    const newCoupon = new Coupon ({
-        code:"GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
-        discountPercentage: 10,
-        expirationDate: new Date(Date.now()+ 30 * 24 * 60 * 60 * 1000),
-        userId: userId,
-    });
+   const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    userId: userId,
+    isActive: true,   
+});
     await newCoupon.save();
 
     return newCoupon;
